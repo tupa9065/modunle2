80ca20c7384245d5a59f1e11a79b9710617607c9
unknown <anhtuphamxt@gmail.com>
2021-04-02 15:00:51 +0700

    khai bao bien

---
+++ khaiBaoBien/src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        int i = 10 ;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.printf("i = %d ",i);
+        System.out.printf("f = %.1f ",f);
+        System.out.printf("d = %.1f ",d);
+        System.out.printf("b = %s ",b);
+        System.out.printf("c= %c ",c);
+        System.out.printf("s = %s ",s);
+    }
+}
