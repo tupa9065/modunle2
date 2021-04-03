3c99785a5df3fe7cbe99ea8695e9c6b5795fa9b4
unknown <anhtuphamxt@gmail.com>
2021-04-03 11:15:16 +0700

    Say oh yeah

---
+++ sayHello/src/hello.java
@@ -0,0 +1,11 @@
+import java.*;
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Enter you Name");
+        Scanner sc =new Scanner(System.in);
+        String name =sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
