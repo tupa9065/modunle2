e204602c8435b6b2245d06bfbecd4007000d1761
unknown <anhtuphamxt@gmail.com>
2021-04-03 10:19:24 +0700

    Check leap year

---
+++ checkYear/src/checkYear.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class checkYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str ="";
+        boolean ck = true;
+        while (ck == true) {
+            System.out.println("Nhập năm bạn muốn kiểm tra");
+            int year = sc.nextInt();
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        str = "Năm nhuận";
+                    } else {
+                        str = "Không phải năm nhuận";
+                    }
+                } else {
+                    str = "Năm nhuận";
+                }
+            } else {
+                str = "Không phải năm nhuận";
+            }
+            System.out.println(str);
+
+            System.out.println("dừng lại nhấn 0, nhấn phím bất kỳ để tiếp tục");
+            int num;
+            num = sc.nextInt();
+            if (num == 0) {
+                ck = false;
+            }
+        }
+    }
+}
