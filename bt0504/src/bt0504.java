008c45acfd1355f0534752e419f904a8a7d6c42f
unknown <anhtuphamxt@gmail.com>
2021-04-05 15:28:04 +0700

    bt 0504

---
+++ bt0504/src/bt0504.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class bt0504 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean ck = true;
+     while (ck){
+         System.out.println("enter the number");
+         int num = sc.nextInt();
+         System.out.println("1. kiểm tra số nguyên tố");
+         System.out.println("2. kiểm tra số chẵn lẻ");
+         System.out.println("3. kiểm tra số chính phương");
+         System.out.println("4. thoát chương trình");
+         int choice = sc.nextInt();
+         switch (choice){
+             case 1:{
+                if(num>2){
+                    boolean prime =true;
+                    for (int i =2 ; i < num ;i++){
+                        if(num%i==0){
+                            prime=false;
+                            break;
+                        }
+                    }
+                    if (prime){
+                        System.out.println(num+" là số nguyên tố");
+                    }else {
+                        System.out.println(num+" không phải số nguyên tố");
+                    }
+                }else {
+                    System.out.println(num+" không là số nguyên tố");
+
+                }
+                 break;
+             }
+             case 2:{
+                 if(num%2==0){
+                     System.out.println(num+" là số chẵn");
+                 }else {
+                     System.out.println(num+" là số lẻ");
+
+                 }
+                 break;
+             }
+             case 3:{
+                 double x = Math.sqrt(num);
+                 if(x-Math.floor(x)==0){
+                     System.out.println(num+" là số chính phương");
+                 }else {
+                     System.out.println(num+" không phải số chính phương");
+
+                 }
+                 break;
+             }
+             case 4:{
+                 ck=false;
+             }
+         }
+     }
+
+    }
+}
--- primeLessThan100/src/primeLessThan100.java
+++ primeLessThan100/src/primeLessThan100.java
@@ -1,9 +1,7 @@
 public class primeLessThan100 {
         public static void main(String[] args) {
-
             String str ="";
             for(int i=2;i<100;i++){
-
                 boolean ck = true;
                 for (int j =2 ; j<i;j++){
                     if(i%j==0){
@@ -14,10 +12,8 @@ public class primeLessThan100 {
                 if(ck){
                     str+=i+", ";
                 }
-
             }
             System.out.println(str);
 
         }
-
 }
