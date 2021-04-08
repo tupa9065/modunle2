64cfc2d2a41b3b09cbc2c255692922d8b5c47a51
unknown <anhtuphamxt@gmail.com>
2021-04-08 14:08:59 +0700

    access modifier

---
+++ accessModifier/src/com/a/A.java
@@ -0,0 +1,5 @@
+package com.a;
+
+public class A {
+    public void msg(){System.out.println("Hello");}
+}
---
+++ accessModifier/src/com/defaulft/Defaulft.java
@@ -0,0 +1,11 @@
+package com.defaulft;
+
+import com.*;
+import com.a.A;
+
+public class Defaulft {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.msg();
+    }
+}
