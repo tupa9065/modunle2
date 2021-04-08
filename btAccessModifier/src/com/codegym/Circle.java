da55f8ba5c7cfca444c3eaa0d021fcd53cd88e99
unknown <anhtuphamxt@gmail.com>
2021-04-08 16:09:50 +0700

    [Bài tập] Access modifier

---
+++ btAccessModifier/src/com/codegym/Circle.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+public class Circle {
+    private double radius =1.0d;
+    private String color="red";
+    public Circle(){ }
+    public Circle(double r){
+        radius =r;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
