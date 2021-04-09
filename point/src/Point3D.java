ca1c11188b1e7b4f545456dae1095caf45723233
unknown <anhtuphamxt@gmail.com>
2021-04-09 16:00:04 +0700

    Point 2d - 3d

---
+++ point/src/Point3D.java
@@ -0,0 +1,38 @@
+public class Point3D extends Point2D{
+    private float z =0.0f;
+    public  Point3D(){}
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                super.toString()+
+                "z=" + z +
+                '}';
+    }
+    public float[] getXYZ(){
+            float [] xyz = {x,y,z};
+            return xyz;
+    }
+    public void setXYZ(float x,float y,float z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
---
+++ point/src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        Point2D draw2D = new Point2D(5,5);
+        //System.out.println(draw2D);
+
+        Point2D draw3D = new Point3D();
+        System.out.println(draw3D);
+    }
+}
