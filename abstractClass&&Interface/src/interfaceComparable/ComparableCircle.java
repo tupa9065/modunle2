f5ff7b0557f803f6312b1152ca53f9d708ff5e0f
unknown <anhtuphamxt@gmail.com>
2021-04-12 14:50:55 +0700

    ComparableCircle

---
+++ abstractClass&&Interface/src/interfaceComparable/ComparableCircle.java
@@ -0,0 +1,21 @@
+package interfaceComparable;
+
+public class ComparableCircle extends Cricle implements Comparable<ComparableCircle>{
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+}
---
+++ abstractClass&&Interface/src/interfaceComparable/Cricle.java
@@ -0,0 +1,33 @@
+package interfaceComparable;
+
+public class Cricle extends Shape{
+    private double radius = 1;
+    public Cricle(){
+    }
+    public Cricle(double radius){
+        this.radius=radius;
+    }
+    public Cricle(double radius,String color, boolean filled){
+        super(color, filled);
+        this.radius=radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI*Math.pow(radius,2);
+    }
+    public double getPerimeter(){
+        return 2*Math.PI*radius;
+    }
+    @Override
+    public String toString() {
+        return "A Circle with radius="+radius+", which is a subclass of "+super.toString();
+    }
+}
