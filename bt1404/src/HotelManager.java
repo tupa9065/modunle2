fa3a70c643e318bbc131b35b220f62fa44282cad
unknown <anhtuphamxt@gmail.com>
2021-04-14 15:22:05 +0700

    bt 1104 Hotel in california

--- bt1404/src/HotelManager.java
+++ bt1404/src/HotelManager.java
@@ -11,7 +11,7 @@ public class HotelManager {
         }
     }
     public void editInformationByCMT(Hotel[] hotels){
-        System.out.println("nhap cmt cua khach ban muon thay doi thong tin");
+        System.out.println("nhap cmt cua khach ");
         String cmt = scanner.nextLine();
         int check=-1;
         for (Hotel hotel:hotels) {
@@ -29,7 +29,7 @@ public class HotelManager {
         }
     }
     public void deleteInformationByCMT(Hotel[] hotels){
-        System.out.println("nhap cmt cua khach ban muon xoa thong tin");
+        System.out.println("nhap cmt cua khach ban ");
         String cmt = scanner.nextLine();
         int check=-1;
         for (Hotel hotel:hotels) {
@@ -54,6 +54,7 @@ public class HotelManager {
                 int hotelPrice=hotel.getRoomCharge();
                 System.out.println("Tien phong "+hotel.getKindOfRoom()+" trong "+hotel.getDateOfHire() +" ngay la :'"+hotelPrice+"' usd");
                 System.out.println("===========================================================================");
+                hotel.deleteInfomationOfRenter();
                 check=1;
                 break;
             }
