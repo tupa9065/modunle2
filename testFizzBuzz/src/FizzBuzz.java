4d45e6ed199cfba7f578e8c2e02f6ad29dac4b96
unknown <anhtuphamxt@gmail.com>
2021-04-14 10:29:38 +0700

    test insert 3 to Fizz

--- testFizzBuzz/src/FizzBuzz.java
+++ testFizzBuzz/src/FizzBuzz.java
@@ -2,6 +2,8 @@ public class FizzBuzz {
     public String fizzBuzz(int number){
         if (number==1) {
             return "one";
+        }else if(number==3){
+            return "Fizz";
         }
         else return number+"";
     }
--- testFizzBuzz/test/FizzBuzzTest.java
+++ testFizzBuzz/test/FizzBuzzTest.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     @Test
-    void fizzBuzz() {
+    void Insert1ToOne() {
         int number = 1;
         String expected="one";
+        String actual= new FizzBuzz().fizzBuzz(number);
+        assertEquals(expected,actual);
+    }
+    @Test
+    void Insert3OutFizz(){
+        byte number =3;
+        String expected="Fizz";
         String actual= new FizzBuzz().fizzBuzz(number);
         assertEquals(expected,actual);
     }
