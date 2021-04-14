b977db4e45a1c8edbb4f240ee66d1193b5f0a658
unknown <anhtuphamxt@gmail.com>
2021-04-14 17:20:18 +0700

    TDD Next day

--- bt_TDD/src/NextDayCalculator.java
+++ bt_TDD/src/NextDayCalculator.java
@@ -1,7 +1,70 @@
-import java.util.Date;
+import java.sql.Date;
 
 public class NextDayCalculator {
-    public int nextDay(int number){
-        return 0;
+
+
+    public static int nextDay(int day, int month, int year) {
+        int nextDay;
+        if (day < 28) {
+            return day + 1;
+        } else if (day == 28) {
+            if (year % 4 == 0) {
+                return day + 1;
+            } else {
+                if(month==2){
+                    return 1;
+                }else {
+                    return day +1;
+                }
+
+            }
+        } else if (day == 29) {
+            if (month==2){
+                if(year%4==0){
+                    return 1;
+                }else {
+                    return -1;
+                }
+
+            }else {
+                return day+1;
+            }
+        }else if (day==30){
+            if (month==2){
+                return -1;
+            }else {
+                switch (month){
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        return day+1;
+
+                }
+                return 1;
+            }
+
+        }else if(day==31){
+            switch (month){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 1;
+
+            }
+            return -1;
+
+        }else {
+            return -1;
+        }
+
     }
+
 }
--- bt_TDD/test/NextDayCalculatorTest.java
+++ bt_TDD/test/NextDayCalculatorTest.java
@@ -4,11 +4,78 @@ import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 class NextDayCalculatorTest {
-    int number;
-    Scanner scanner = new Scanner(System.in);
-    @Test
-    void nextDay() {
-        number = 1;
 
+    @Test
+    void nextDayTest01012001() {
+        int day = 1;
+        int mont =1;
+        int year=2001;
+        int expected = 2;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
     }
+    @Test
+    void nextDayTest31012001() {
+        int day = 31;
+        int mont =1;
+        int year=2001;
+        int expected = 1;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest31072001() {
+        int day = 31;
+        int mont =7;
+        int year=2001;
+        int expected = 1;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest30012001() {
+        int day = 30;
+        int mont =1;
+        int year=2001;
+        int expected = 31;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest29022021() {
+        int day = 31;
+        int mont =1;
+        int year=2001;
+        int expected = -1;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest29022020() {
+        int day = 29;
+        int mont =2;
+        int year=2020;
+        int expected = 1;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest31122020() {
+        int day = 31;
+        int mont =12;
+        int year=2020;
+        int expected = 1;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+    @Test
+    void nextDayTest15022020() {
+        int day = 15;
+        int mont =2;
+        int year=2020;
+        int expected = 16;
+        int result = NextDayCalculator.nextDay(day,mont,year);
+        assertEquals(expected, result);
+    }
+
 }
