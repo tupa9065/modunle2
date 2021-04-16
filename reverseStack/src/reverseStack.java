364aff654871b6085411b7fe88f5bfa188ad61fc
unknown <anhtuphamxt@gmail.com>
2021-04-16 16:18:07 +0700

    reverse Stack

--- reverseStack/src/reverseStack.java
+++ reverseStack/src/reverseStack.java
@@ -3,24 +3,14 @@ import java.util.Stack;
 
 public class reverseStack {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        System.out.println(stack);
+        reverseInt();
 
-        int[] arraysInt = new int[stack.size()];
-        int index=0;
-        for (int i:stack) {
-            arraysInt[index]=i;
-            index++;
-        }
-        stack.clear();
-        for (int i=arraysInt.length-1;i>=0;i--) {
-            stack.push(arraysInt[i]);
-        }
-        System.out.println(stack);
+        reverseStr();
+
+
+    }
+
+    private static void reverseStr() {
         String str="thiên hạ vô địch";
         Stack<Character> wStack = new Stack<>();
         char[] arraysStr = str.toCharArray();
@@ -33,9 +23,25 @@ public class reverseStack {
         }
         System.out.println(str);
         System.out.println(reverseStr);
+    }
 
-
-
-
+    private static void reverseInt() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack);
+        int[] arraysInt = new int[stack.size()];
+        int index=0;
+        for (int i:stack) {
+            arraysInt[index]=i;
+            index++;
+        }
+        stack.clear();
+        for (int i=arraysInt.length-1;i>=0;i--) {
+            stack.push(arraysInt[i]);
+        }
+        System.out.println(stack);
     }
 }
