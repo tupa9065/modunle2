4a4da1c65f4864f0bf467517bc8e797b0fcaded8
unknown <anhtuphamxt@gmail.com>
2021-04-16 15:53:28 +0700

    reverse Stack

---
+++ reverseStack/src/reverseStack.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class reverseStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack);
+
+        int[] arraysInt = new int[stack.size()];
+        int index=0;
+        for (int i:stack) {
+            arraysInt[index]=i;
+            index++;
+        }
+        stack.clear();
+        for (int i=arraysInt.length-1;i>=0;i--) {
+            stack.push(arraysInt[i]);
+        }
+        System.out.println(stack);
+        String str="thiên hạ vô địch";
+        Stack<Character> wStack = new Stack<>();
+        char[] arraysStr = str.toCharArray();
+        for (char i:arraysStr) {
+            wStack.push(i);
+        }
+        String reverseStr="";
+        for (char i:arraysStr) {
+            reverseStr +=wStack.pop();
+        }
+        System.out.println(str);
+        System.out.println(reverseStr);
+
+
+
+
+    }
+}
