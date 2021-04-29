285e3bfd968c9ef18dd5700ab306ae43752fdca8
Admin <anhtuphamxt@gamil.com>
2021-04-29 14:47:42 +0700

    2904 add REGEX_CLASSNAME

--- Strudent/src/Main.java
+++ Strudent/src/Main.java
@@ -3,12 +3,15 @@ import java.util.regex.Pattern;
 
 public class Main {
     static final String REGEX_CLASSNAME="^[CAP]\\d{4}[GHIKLM]\\d{1}$";
+    static final String REGEX_TELEPHONE="^\\d{3}[-]\\d{7}$";
     public static boolean checkValidate(String str,String regex){
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
         return matcher.find();
     }
     public static void main(String[] args) {
-        System.out.println(checkValidate("C0221H1",REGEX_CLASSNAME));
+        System.out.println(checkValidate("CA0221H1",REGEX_CLASSNAME));
+        System.out.println(checkValidate("098-9380777",REGEX_TELEPHONE));
+
     }
 }
