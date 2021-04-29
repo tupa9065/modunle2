901894ba46aa7bb52a949c0f861b5799be3ea70b
Admin <anhtuphamxt@gamil.com>
2021-04-29 11:47:56 +0700

    2904 validate email

---
+++ ValidateEmail/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] emails = {"abc@xyz.com", "abc.com", "abc@xyz"};
+        String regex = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher;
+        for (int i = 0; i < emails.length; i++) {
+            matcher= pattern.matcher(emails[i]);
+            if(matcher.find()){
+                System.out.println(emails[i]);
+            }
+
+        }
+    }
+}
