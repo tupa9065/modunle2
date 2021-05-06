26000f9d4db6847ef5a1b058f5cc3fe689fa6659
Admin <anhtuphamxt@gamil.com>
2021-05-06 15:02:15 +0700

    06/05

---
+++ Taxi Pool/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static final int NUM_OF_CLIENT = 8;
+
+    public static void main(String[] args) {
+        TaxiPool taxiPool = new TaxiPool();
+        for (int i = 1; i < NUM_OF_CLIENT; i++) {
+            Runnable client = new TaxiThread(taxiPool);
+            Thread thread = new Thread(client);
+            thread.start();
+        }
+    }
+}
---
+++ Taxi Pool/src/com/company/TaxiNotFoundException.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TaxiNotFoundException extends RuntimeException {
+
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
